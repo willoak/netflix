b2631cb7dafce21e2111baf895e781bb2a986098
Willian Carvalho <willss_oak@hotmail.com>
2020-08-25 15:04:30 -0300

    listando os filmes

--- src/FilmesItem.js
+++ src/FilmesItem.js
@@ -1,20 +1,26 @@
 import React from 'react';
+import './filmesItem.css';
 
 const FilmesItem = ({title, items}) => {
 
+    let tamanho = document.querySelector(".movie-item");
+
+    console.log( tamanho )
+
     return (
-        <div className="item">
+        <div className="movie-row">
             <h2>{title}</h2>
-            <div className="movie-row">
-                {
-                    items.results.length > 0 && items.results.map( (item, key) => (
-                        <img key={key} src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_name}/>
-                    ))
-
-                }
+            <div className="movie-area">
+                <div className="movie-list">
+                    {
+                        items.results.length > 0 && items.results.map( (item, key) => (
+                            <div key={key} className="movie-item">
+                                <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_name}/>
+                            </div>
+                        ))
+                    }
+                </div>
             </div>
-
-
         </div>
     )
 }
